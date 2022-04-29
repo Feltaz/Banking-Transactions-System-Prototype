a47ea0e0f941fbb63f1b0690cc91860216e892bf
feltaz <ouerfelli.moetaz@gmail.com>
2022-04-29 03:11:18 +0100

    updated client and slight modification to main.c

--- client/client.py
+++ client/client.py
@@ -20,29 +20,27 @@ def start_connection(HOST,PORT):
       #  response=s.recv(1024)
    # except OSError:
        # print("theres a problem with the connection shuting down...")
+    choice=""
     while True:
         operations=[]
-        confirm=""
-        while True :
-            operation=menu.transaction_menu()
-            operations.append(operation)
-            print("these are your operations so far: ")
-            #print(operations)
-            for operation in operations:
-                print(f"you are gonna {operation[0]} {operation[2]} from  the account of reference {operation[1]}")
-            print("press y if you want to confirm your operations, press anything else to make other operations")
-            confirm=input()
-            if confirm == "y":
-                break
-            else:
-                continue
+        operation=menu.transaction_menu()
+        operations.append(operation)
+        print("these are your operations so far: ")
+        #print(operations)
+        for operation in operations:
+            print(f"you are gonna {operation[0]} {operation[2]} from  the account of reference {operation[1]}")
         requests=[":".join(operation) for operation in operations]
         for request in requests:
             s.sendall(request.encode())
             response=s.recv(1024)
             print(response.decode('utf-8'))
-        
-        break
+        while choice!="y" and choice!="q":
+            print("do you wish to make another transaction? press y to conitnue or q to quit")
+            choice=input()
+        if(choice=="y"):
+            continue    
+        elif choice=="q":
+            break
     
 
 def main():
